3552fcc3ff9ebddc650000678f91191fd3450210
saso <s.starace99@gmail.com>
2022-01-01 22:46:24 +0100

    Hash table structure

---
+++ HashTable.h
@@ -0,0 +1,33 @@
+//
+// Created by saso on 01/01/2022.
+//
+
+#ifndef HASHGRAPH_HASHTABLE_H
+#define HASHGRAPH_HASHTABLE_H
+
+
+#include <list>
+#include <ostream>
+#include "Graph/TreeNode.h"
+
+class HashTable {
+private:
+    TreeNode **table;
+    TreeNode *dummy{};
+    int capacity;
+    int size;
+
+public:
+    HashTable(int bucketNo);
+
+    int hashCode(int key) const;
+
+    void insertNode(TreeNode &treeNode);
+
+    TreeNode *operator[](int) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const HashTable &table);
+};
+
+
+#endif //HASHGRAPH_HASHTABLE_H
